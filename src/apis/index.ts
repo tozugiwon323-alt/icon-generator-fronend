799fb062ceb304a934bdd32c6e5cf52349faf068
tozugiwon323-alt <tozugiwon323@gmail.com>
2025-11-21 13:58:00 +0700

    change server url

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 
+const serverUrl = "https://api.life-sui.com"
+// const serverUrl = "http://localhost:3001/"
+
 export const generateIcons = async (data: { prompt: string, style: string, colors: string[] }) => {
-    const response = await axios.post('/api/generate', data);
+    const response = await axios.post(serverUrl + '/api/generate', data);
     return response.data
 }
